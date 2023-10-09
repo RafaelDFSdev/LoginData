f0d54c0a40f6f4d98835ee924bbe90c50c44ba4e
xghost2021 <rafaeldias44k@gmail.com>
2023-10-08 22:09:07 -0300

    favicon commit

--- cadastrosucesso.php
+++ cadastrosucesso.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/cadastro.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../LoginData/img/favicon.ico" type="image/x-icon">
     <title>Trabalho Gilmar</title>
 </head>
 
